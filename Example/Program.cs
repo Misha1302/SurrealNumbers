8924d67b23b0aed9474332fdb78b1176172811ef
Михаил <77919295+Misha1302@users.noreply.github.com>
2025-09-07 00:25:58 +0300

    added tests

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,24 +1,6 @@
-﻿using System.Diagnostics;
-using SurrealNumber;
+﻿using SurrealNumber;
+using static SurrealNumber.SurrealCacheNumbers;
 
-var minOne = SurrealCacheNumbers.MinusOne;
-var zero = SurrealCacheNumbers.Zero;
-var one = SurrealCacheNumbers.One;
-
-var arr = (Span<SurrealNum>) [minOne, zero, one];
-//
-// foreach (var a in arr)
-// foreach (var b in arr)
-//     Console.WriteLine($"{a.To<double>()} {b.To<double>()} {(a + b).To<double>()} {(a + b).To<string>()}");
-
-var sw = Stopwatch.StartNew();
-var big = zero;
-for (var i = 1; i <= 300; i++) big += one;
-Console.WriteLine(sw.ElapsedMilliseconds + "ms");
-
-
-sw = Stopwatch.StartNew();
-big += big;
-Console.WriteLine(sw.ElapsedMilliseconds + "ms");
-
-Console.WriteLine(big.ConvertToDouble());
+var arr = (SurrealNum[]) [MinusOne, Zero, MinusOne + One, One, One + One];
+var res = arr.SelectMany(_ => arr, (a, b) => a + b);
+Console.WriteLine(string.Join(", ", res.Select(x => x.To<double>())));
--- SurrealArithmeticTests/UnitTest1.cs
+++ SurrealArithmeticTests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using SurrealNumber;
+using static SurrealNumber.SurrealCacheNumbers;
+
 namespace SurrealArithmeticTests;
 
 public class Tests
@@ -10,6 +13,24 @@ public class Tests
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        var arr = (SurrealNum[]) [MinusOne, Zero, One, One + One, MinusOne + One];
+        var res = arr.SelectMany(_ => arr, (a, b) => a <= b);
+        var ans = (List<bool>)
+        [
+            true, true, true, true, true, false, true, true, true, true, false, false, true, true, false, false, false,
+            false, true, false, false, true, true, true, true,
+        ];
+
+        Assert.That(res, Is.EqualTo(ans));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var arr = (SurrealNum[]) [MinusOne, Zero, MinusOne + One, One, One + One];
+        var res = arr.SelectMany(_ => arr, (a, b) => a + b).Select(x => x.To<double>());
+        var ans = (List<int>) [-2, -1, -1, 0, 1, -1, 0, 0, 1, 2, -1, 0, 0, 1, 2, 0, 1, 1, 2, 3, 1, 2, 2, 3, 4];
+
+        Assert.That(res, Is.EqualTo(ans));
     }
 }
